6893ae53c116f8803d58d4e7fa3789d8fa1c5851
Jessica <jessica1329b@gmail.com>
2022-04-20 16:49:58 -0400

    ignored appsettings

---
+++ ORMandDapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORMandDapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
